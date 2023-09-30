7454f4cae3a73f71d5208089aefba4b57a69a19b
MH <alaausama039@gmail.com>
2023-09-30 15:45:31 +0300

    radio finished

--- lib/radio/radio_view.dart
+++ lib/radio/radio_view.dart
@@ -5,6 +5,45 @@ class RadioView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    var them = Theme.of(context);
+    return Container(
+      color: Colors.transparent,
+      height: double.infinity,
+      width: double.infinity,
+      child: Container(
+        child: Column(
+          children: [
+            SizedBox(height: 100),
+            Image.asset("assets/images/radio_image.png"),
+            SizedBox(height: 40),
+            Text(
+              "إذاعة القرآن الكريم",
+              style: them.textTheme.bodyMedium,
+            ),
+            SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.skip_previous_rounded,
+                  size: 50,
+                  color: them.primaryColor,
+                ),
+                Icon(
+                  Icons.pause,
+                  size: 50,
+                  color: them.primaryColor,
+                ),
+                Icon(
+                  Icons.skip_next_rounded,
+                  size: 50,
+                  color: them.primaryColor,
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
